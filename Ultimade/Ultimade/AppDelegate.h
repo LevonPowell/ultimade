fce64a79190ac3d3acf51449e37066d2679fd737
LevonPowell <levon.zadravec.powell@gmail.com>
2012-12-05 18:21:21 -0800

    Added XCode Project
    Added XCode iOS Project files. Created an empty application and created
    controllers, models, and views folders.

---
+++ Ultimade/Ultimade/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ultimade
+//
+//  Created by Levon Powell on 2012-12-05.
+//  Copyright (c) 2012 Levon Powell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ultimade/Ultimade/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ultimade
+//
+//  Created by Levon Powell on 2012-12-05.
+//  Copyright (c) 2012 Levon Powell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
